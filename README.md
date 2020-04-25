238c9ce01e05c192c1abe61df7a8b7fd70045b5c
Jaime Huynh <taihuynh0901@gmail.com>
2020-04-25 04:01:54 -0700

    Code Quiz with Timer

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Code-Quiz
 Timed code quiz with multiple-choice question
+
+Click Start button to start the quiz.
+
+Everyone will have 75 second to complete 5 question.
+
+Each wrong question will subtract the time by 10 secs.
+
+After completing the quiz, user have option to enter name and save their highscore.
+
+User could also hit 'Go Back' to do the quiz again or 'clear your score' to clear all the score.
---
+++ script.js
@@ -0,0 +1,153 @@
+// Define and initialize variable
+let quizArray = [
+    {
+        question: 'Commonly used data types DO NOT include:',
+        choice: ['1. strings', '2. booleans', '3. alerts','4. numbers']
+    },
+
+    {
+        question: "What property can you use to change an element's class name with JavaScript?",
+        choice: ['1. style', '2. className', '3. node','4. class']
+    },
+
+    {
+        question: 'Which method can be used to remove an element from the DOM?',
+        choice: ['1. removeChild', '2. removeElement', '3. removeNode','4. removeParent']
+    },
+
+    {
+        question: 'What is one way to think of an array?',
+        choice: ['1. As a racetrack ', '2. As a box of chocolate', '3. As a shopping list','4. As a carton of milk']
+    },
+
+    {
+        question: 'Which array method adds an item to the end of an array?',
+        choice: ['1. shift', '2. unshift', '3. pop','4. push']
+    },
+  
+];
+
+var myKey =['1. strings', '2. className', '1. removeChild','3. As a shopping list', '4. push' ];
+
+var questIndex = 0;
+
+var timer = 75;
+
+var highScore = 0;
+
+var startBtn = $(".btn-start");  //select the start button
+
+// Retrieve the highScore from local Storage
+var savedHighScore = JSON.parse(localStorage.getItem('highScore'));
+if (savedHighScore !== null) {
+    highScore = savedHighScore;
+}
+
+
+
+startBtn.on('click', () => {
+    $(".welcome").addClass("d-none");
+    $(".quiz").removeClass("d-none");
+    setTime();
+    renderQuiz();
+});
+
+// Choose multiple choice answer for each quiz question
+$(".quiz").on('click', (event) => {
+    if (event.target.tagName == 'BUTTON') {
+        var myChoice = $(event.target).text();
+        if (myChoice == myKey[questIndex]) {
+            $('h3').text('Correct!');
+            $('.multiChoice').empty();
+            nextQuestion();
+        }
+        else {
+            $('h3').text('Wrong!');
+            if (timer < 10) {
+                timer = 0;
+            }
+            else {
+                timer -= 10;
+            }
+            $('.multiChoice').empty();
+            nextQuestion();
+        }
+    }
+
+});
+
+// ---Render Quiz---//
+function renderQuiz () {
+    if(questIndex <= 4 && timer > 0){
+        $(".question").text(quizArray[questIndex].question);
+        var multiChoice = quizArray[questIndex].choice;
+        for (let i = 0; i < multiChoice.length; i++) {
+            var button = $("<button>");
+            button.addClass("btn-lg btn-info d-inline mb-2");
+            button.text(multiChoice[i]);
+            $(".multiChoice").append(button);
+        }
+    }
+}
+
+// ---Render next question---//
+function nextQuestion () {
+    if (questIndex < 5) {
+        questIndex++;
+        renderQuiz();
+    }
+    else {
+        alert ('You are done!');
+    }
+}
+
+// ---Set up timer--//
+function setTime() {
+    var timeInterval = setInterval( () => {
+        timer--;
+        $('.timer').text(timer);
+    if (questIndex >=5 || timer === 0) {
+        highScore = timer;
+        clearInterval(timeInterval);
+        $(".score").text(highScore);
+        $(".quiz").addClass("d-none");
+        $(".savedScore").removeClass("d-none");
+        localStorage.setItem('highScore', JSON.stringify(highScore));
+    }
+
+    else if (timer < 0) {
+        timer = 0;
+        highScore = timer;
+        clearInterval(timeInterval);
+        $(".score").text(highScore);
+        
+        $(".savedScore").removeClass("d-none");
+        localStorage.setItem('highScore', JSON.stringify(highScore));
+    }
+    },1000);
+}
+
+// ---Enter name and Submit---//
+$('.submit').on('click', () => {
+    var myScore = `${$(".nameInput").val()} - ${highScore}`;
+    var li = $('<li>');
+    li.text(myScore);
+    li.addClass("bg-success list-group-item text-white mb-3");
+    $('.scoreList').append(li);
+    $(".savedScore").addClass("d-none");
+    $(".scoreBoard").removeClass("d-none");
+
+});
+
+//---Clear scoreBoard---//
+$('.clear').on('click', () => {
+    $('.scoreList').empty();
+});
+
+//---Go Back or Start Game again---//
+$('.goBack').on('click', () => {
+    questIndex = 0;
+    timer = 75;
+    $(".scoreBoard").addClass("d-none");
+    $(".welcome").removeClass("d-none");
+});
